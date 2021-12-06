4062bd2b583baec1238be85ff1190cf4987bb4af
vikassinghpol <93115285+vikassinghpol@users.noreply.github.com>
2021-12-06 11:22:30 +0530

    apsx

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+
+// var get_side = document.getElementById('accordionSidebar');
+
+// function side_onload(){
+// 	get_side.classList.add("mobil_vie");
+// }
+
+
+// var mobl_fullside = document.getElementById('accordionSidebar');
+
+// function side_tog() {
+// 	mobl_fullside.classList.toggle("mobil_vie")
+// }
+
+// var get_side = document.getElementById('accordionSidebar');
+
+// function func() {
+// 	get_side.classList.toggle("visi_hidd");
+// }
